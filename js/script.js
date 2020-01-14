e3ffa58179242e24ea1f92abbec7217ad291a0b8
unknown <saurab.shrestha99@gmail.com>
2020-01-14 16:33:12 +0545

    up to effect animations

---
+++ js/script.js
@@ -0,0 +1,73 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);
+  // $(".green-box").fadeOut(2000);
+  // $(".red-box").fadeIn(2000);
+  // $(".red-box").fadeTo(2000,0.5);
+  // $(".red-box").fadeTo(2000,0.2);
+  // $(".green-box").fadeTo(2000,0.5);
+  // $(".blue-box").fadeTo(2000,0.5);
+  // $(".blue-box").fadeToggle();
+  // $(".blue-box").fadeToggle();
+  // $(".blue-box").hide(2000);
+  // $(".blue-box").show(2000);
+
+  // slide
+  // $(".blue-box").slideUp(2000);
+  // $(".blue-box").slideDown(2000);
+  // $(".blue-box").slideToggle(2000);
+
+  // $('p').hide(2000);
+  // $('p').slideDown();
+
+  // moving elements
+  // $(".blue-box").animate({
+  //   "margin-left": "+=200px"
+  // }, 5000, "linear");
+
+  // $(".blue-box").animate({
+  //   "margin-left": "-=200px"
+  // }, 5000, "linear");
+
+  // custom Animation
+  
+  // $(".blue-box").animate({
+  //   marginLeft: "200px",
+  //   "opacity":"0",
+  //   "height":"50px",
+  //   width:"50px",
+  //   "margin-top": "25px"
+
+  // }, 1000);
+
+  // $("p").animate({
+  //   // marginLeft: "200px",
+    
+  //   "font-size":"+=8px",
+    
+  //   // width:"+=50px",
+  //   // "margin-top": "25px"
+
+  // }, 1000);
+
+  // $(".red-box").fadeTo(1000, 0.2);
+  // $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  // $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(1000).fadeIn();
+// // callback function
+//     $(".red-box").fadeTo(1000, 0.2, function(){
+//       alert("This is callback: Animation finished");
+//     });
+
+  $(".lightbox").delay(500).fadeIn(1000);
+
+
+
+
+
+
+
+
+
+});
